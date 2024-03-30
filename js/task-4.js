d21cae058047b5522031b3c925180c59815e506b
nekodndz <138133074+nekodndz@users.noreply.github.com>
2024-03-30 09:50:34 +0200

    3

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector('.login-form');
+const emailInput = loginForm.elements['email'];
+const passwordInput = loginForm.elements['password'];
 
+loginForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const emailInput = emailInput.value.trim();
+  const passwordInput = passwordInput.value.trim();
+
+  if (emailInput === '' || passwordInput=== '') {
+    alert('All form fields must be filled in');
+  } else {
+      const formData = {
+        email: emailInput,
+        password: passwordInput
+      };
+
+      console.log(formData);
+      loginForm.reset();
+    }
+});
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,38 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
+
+function createBoxes(amount) {
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement('div');
+    const size = 30 + i * 10;
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    fragment.appendChild(div);
+  }
+  boxesContainer.appendChild(fragment);
+}
+
+createButton.addEventListener('click', () => {
+  const amount = parseInt(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = '';
+  } else {
+    alert('Please enter a number between 1 and 100.');
+  }
+});
+
+destroyButton.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
